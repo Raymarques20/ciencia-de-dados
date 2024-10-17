b36874cb1decf821c627d87e7ce95e5780bd880f
Raymarques20 <105437908+Raymarques20@users.noreply.github.com>
2024-10-17 10:10:07 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,17 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformaçoesGlobais() {
-   const res = await fetch(url)
-   const dados = res.json()
-   console.log(dados);
- }
- 
-visualizarInformaçoesGlobais();
+async function visualizarInformacoesGlobais() {
+  const res = await fetch(url)
+  const dados = res.json()
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  paragrafo.innerHTML = `Você sabia que o mundo tem 
+  ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
+  ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média 
+  ${dados.tempo_medio} horas conectadas.`
+  
+  const containder = document.getElementById('graficos-container')
+  containder.appendChild(paragrafo)
+}
+
+visualizarInformacoesGlobais();
