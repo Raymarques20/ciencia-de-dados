6ccc06fa04dc0655bc176e840f00c0f8755a6b55
Raymarques20 <105437908+Raymarques20@users.noreply.github.com>
2024-10-17 10:22:02 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,13 +5,10 @@ async function visualizarInformacoesGlobais() {
   const dados = res.json()
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
-  paragrafo.innerHTML = `Você sabia que o mundo tem 
-  ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
-  ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média 
-  ${dados.tempo_medio} horas conectadas.`
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média   ${dados.tempo_medio} horas conectadas.`
   
-  const containder = document.getElementById('graficos-container')
-  containder.appendChild(paragrafo)
+  const container = document.getElementById('graficos-container')
+  container.appendChild(paragrafo)
 }
 
 visualizarInformacoesGlobais();
