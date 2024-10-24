6765b2bed48a7c4e3945d4d96ee9bce8c04cc269
Raymarques20 <105437908+Raymarques20@users.noreply.github.com>
2024-10-24 10:37:49 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,10 +6,12 @@ async function visualizarInformacoesGlobais() {
   const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
   const pessoaNoMundo = (dados.total_pessoas_no mundo) / 1e9
   const horas = parseInt(dados.tempo_medio)
-
+  const minutos = Math.round(( dados.tempo-medio -horas) *100)
+  const porentagemConectada = ((pessoasConetadas  /  pessoaNoMundo) * 100).toFixed(2)
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
   paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span>${horas} </span> horas e <span>${minutos}</span> minutos-conectadas.`
+  <br> Isso significa que aproximadamente ${porcentagemConetada} de pessoas estão conectadas em alguma rde SpeechRecognitionAlternative.
   
   const container = document.getElementById('graficos-container')
   container.appendChild(paragrafo)
